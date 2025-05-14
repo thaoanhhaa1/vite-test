89568d8bde63eef56fb59ac1271df84a0790d330
Ha Anh Thao <thaoanhhaa1@gmail.com>
2025-05-14 16:47:40 +0700

    Xóa nhiều thành phần không cần thiết và cập nhật giao diện chính của ứng dụng. Thay thế nội dung trong App.tsx bằng tiêu đề "Hello World".

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,7 @@
-import Layout from './components/blog/Layout';
-import PostDetail from './components/blogDetail/PostDetail';
-import CreatePost from './components/createPost/CreatePost';
-
 function App() {
     return (
         <>
-            {/* <MajorDetail
-                major={{
-                    bannerImage:
-                        'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
-                    name: 'Software Engineering',
-                    shortDescription: 'Software Engineering',
-                    overview: 'Software Engineering',
-                    careerOpportunities: [
-                        {
-                            title: 'Software Engineer',
-                            description: 'Software Engineer',
-                        },
-                    ],
-                    workshops: [
-                        {
-                            id: '1',
-                            image: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
-                            title: 'Software Engineer',
-                            status: 'Software Engineer',
-                            date: 'Software Engineering',
-                            time: 'Software Engineering',
-                            location: 'Software Engineering',
-                            attendees: 'Software Engineering',
-                            description: 'Software Engineering',
-                            isFree: 'Software Engineering',
-                            price: 'Software Engineering',
-                        },
-                    ],
-                    id: '1',
-                }}
-            />
-            <WorkshopList2 />
-            <MajorsPage />
-            <Membership />
-            <PaymentDetail />
-            <PaymentPage />
-            <WorkshopPage /> */}
-            {/* <HeroSection />
-            <Layout>
-                <PostGrid />
-            </Layout> */}
-
-            {/* <PostDetail /> */}
-            <CreatePost />
+            <h1>Hello World</h1>
         </>
     );
 }
